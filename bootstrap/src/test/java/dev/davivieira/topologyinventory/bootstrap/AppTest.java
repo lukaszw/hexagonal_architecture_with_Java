dced827257b0865a7d7a8780b14bcb4ab4133c58
Łukasz Wiśniewski <l.wis@o2.pl>
2024-10-20 21:13:27 +0200

    chapter 10 (bootstrap)

---
+++ bootstrap/src/test/java/dev/davivieira/topologyinventory/bootstrap/AppTest.java
@@ -0,0 +1,10 @@
+package dev.davivieira.topologyinventory.bootstrap;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+class AppTest {
+
+}
